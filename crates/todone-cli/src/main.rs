698f1a1e26f79e1495f6b4c6af99a45b860a4ff5
Federico Fontana <federico.fontana2000@gmail.com>
2026-08-17 13:00:29 +0200

    todo: add todos

--- crates/todone-cli/src/main.rs
+++ crates/todone-cli/src/main.rs
@@ -16,6 +16,46 @@ use clap::Parser;
 
 use crate::cli::{Cli, Command};
 
+// TODO: should really change the template for the issue file: first h1 is the title of the issue, everything else is the content. Might even make the title part of the frontmatter, and have the description be everything after it. Might also change the frontmatter to include a comment explaining what to do? Might also Move the content that is auto included to a comment that is not included in the final issue? Maybe configurable
+// TODO: currently if the edit breaks the format the whole app quits, let's just void the change, go back to the tui in the same spot and notify the user
+// TODO: move some info from the top bar to the bottom bar
+// TODO: add some color and nicer in the final page
+// TODO: streaming parsing (we lose the total number at start, but can start faster. Maybe move
+// parsing to other thread so it can go on while the user looks at the first part?)
+// TODO: change `o view` in bottom bar to `o open` and don't promise it to be readonly, since we
+// already have the movable preview
+// TODO: mouse && scroll support. Currently after using the mouse everything feels off and sluggish
+// TODO: react to file changes. Must notify the user if a previously selected TODO is removed/moved via external file change
+// TODO: give option to consider the content right after `// TODO:` as the title, and the lines contigous to it (but still in the same comment) as the initial content of the issue
+// TODO: move to `turdo` package
+// TODO: better help (floating) menu
+// TODO: progress floaging page with navigation
+// FIXME: this should not be recognized as todo
+// ```sh
+// │ > 251 │ /// "// TODO: fix\n",                                                                                                                                                                                                                               │
+// │ > 252 │ /// ).unwrap();                                                                                                                                                                                                                                     │
+// │ > 253 │ /// assert_eq!(comments.len(), 1);                                                                                                                                                                                                                  │
+// │ > 254 │ /// assert_eq!(comments[0].line, 1);                                                                                                                                                                                                                │
+// │ > 255 │ /// ```
+// ```
+// // FIXME: also seems like we lose indentation in comments
+// ```
+// │   242 │ /// ```                                                                                                                                                                                                                                             │
+// │   243 │ /// use std::path::Path;                                                                                                                                                                                                                            │
+// │   244 │ /// use todone_core::language;                                                                                                                                                                                                                      │
+// │   245 │ /// use todone_core::scan::parse_comment_nodes;                                                                                                                                                                                                     │
+// │   246 │ ///                                                                                                                                                                                                                                                 │
+// │   247 │ /// let lang = language::by_extension(Path::new("a.rs")).unwrap();                                                                                                                                                                                  │
+// │   248 │ /// let comments = parse_comment_nodes(                                                                                                                                                                                                             │
+// │   249 │ /// Path::new("a.rs").into(),                                                                                                                                                                                                                       │
+// │   250 │ /// lang,                                                                                                                                                                                                                                           │
+// │ > 251 │ /// "// TODO: fix\n",                                                                                                                                                                                                                               │
+// │ > 252 │ /// ).unwrap();                                                                                                                                                                                                                                     │
+// │ > 253 │ /// assert_eq!(comments.len(), 1);                                                                                                                                                                                                                  │
+// │ > 254 │ /// assert_eq!(comments[0].line, 1);                                                                                                                                                                                                                │
+// │ > 255 │ /// ```
+// ```
+
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
     let color = resolve_color(cli.color, cli.no_color, std::io::stdout().is_terminal());
